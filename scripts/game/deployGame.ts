5be2cf1c7bef5b07b17b16a186390b22817c1521
vodoanthanh <vodoanthanh093@gmail.com>
2022-04-29 06:18:57 +0700

    Handle coins

--- scripts/game/deployGame.ts
+++ scripts/game/deployGame.ts
@@ -2,10 +2,11 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { ethers, upgrades } from 'hardhat';
 
 const deployGameUpgradeable = async (deployer: SignerWithAddress) => {
-    const upgradeableFactory = await ethers.getContractFactory("GameUpgradeable", deployer);
+    const upgradeableFactory = await ethers.getContractFactory('GameUpgradeable', deployer);
     const proxyInstance = await upgrades.deployProxy(upgradeableFactory);
     await proxyInstance.deployed();
-    console.log("GameUpgradeable: ", proxyInstance.address);
+    console.log('Game proxy\'s address: ', proxyInstance.address);
+
     return proxyInstance;
 };
 export default deployGameUpgradeable;
--- scripts/game/upgradeGame.ts
+++ scripts/game/upgradeGame.ts
@@ -2,11 +2,11 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { ethers, upgrades } from 'hardhat';
 
 const upgradeGameUpgradeable = async (baseAddress: string, deployer: SignerWithAddress, version: string = 'GameUpgradeableV2') => {
-    const GameUpgradeableV2Factory = await ethers.getContractFactory(version, deployer);
-    const GameUpgradeableV2Instance = await upgrades.upgradeProxy(baseAddress, GameUpgradeableV2Factory);
+    const GameUpgradeableFactory = await ethers.getContractFactory(version, deployer);
+    const GameUpgradeableInstance = await upgrades.upgradeProxy(baseAddress, GameUpgradeableFactory);
     console.log('GameUpgradeable upgraded');
 
-    return GameUpgradeableV2Instance;
+    return GameUpgradeableInstance;
 };
 
 export default upgradeGameUpgradeable;
