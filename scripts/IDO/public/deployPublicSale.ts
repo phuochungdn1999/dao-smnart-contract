41e049c85f2eb667b2dc824c466dbe53aca368b5
shivannguyen <shivannguyen259999@gmail.com>
2022-09-20 11:01:05 +0700

    update vesting

---
+++ scripts/IDO/public/deployPublicSale.ts
@@ -0,0 +1,12 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { ethers, upgrades } from 'hardhat';
+
+const deployPublicSalesUpgradeable = async (deployer: SignerWithAddress) => {
+    const upgradeableFactory = await ethers.getContractFactory('PublicSalesUpgradeableUpgradeable', deployer);
+    const proxyInstance = await upgrades.deployProxy(upgradeableFactory);
+    await proxyInstance.deployed();
+    console.log('PublicSales proxy\'s address: ', proxyInstance.address);
+
+    return proxyInstance;
+};
+export default deployPublicSalesUpgradeable;
